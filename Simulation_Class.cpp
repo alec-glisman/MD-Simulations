28c27945e8c6006932541a07bfbda00454fdec78
Alec Glisman <alec.glisman@gmail.com>
2018-12-29 16:32:22 -0800

    Added second constructor with simulation variables input and added that to main.cpp as well for init

--- Simulation_Class.cpp
+++ Simulation_Class.cpp
@@ -5,20 +5,26 @@
 #include <iostream>
 #include <vector>
 
+// TYPE ALIASES
+using doubleMatrix_t =  std::vector<std::vector<double> >;
+using doubleVector_t =  std::vector<double>;
+using intMatrix_t =  std::vector<std::vector<int> >;
+using intVector_t =  std::vector<int>;
+
+
 
 class Simulation
 {
 
-public:
-    // Public variables and methods
-
-
 private:
-    // Log other filesystem information
+
+    // Log and other filesystem information
     std::string m_folder = "Q3.1-Energy";
     std::string m_filename = "sim00";
-    // Simulation parameters that must be input by the constructor or will take these default values
+
     unsigned long m_n_dimensions = 3;
+
+    // Simulation parameters that must be input by the constructor or will take these default values
     int m_num_iter = 4000;
     int m_num_simulations = 6;
     unsigned long m_n_particle = 50;
@@ -28,27 +34,50 @@ private:
     double m_sigma = 1.0;
     double m_dt = 0.005;
     double m_t_total = 0.8;  // Overwritten later
+
     // Constant box properties
     double m_vol = m_box * m_box * m_box;
     double m_rho = m_n_particle / m_vol;
+
     // Kinematic Variables
-    std::vector<std::vector<double> > radii {m_n_particle, std::vector<double>(m_n_dimensions)};
-    std::vector<std::vector<double> > velocities {m_n_particle, std::vector<double>(m_n_dimensions)};
+    doubleMatrix_t radii{m_n_particle, doubleVector_t(m_n_dimensions)};
+    doubleMatrix_t velocities{m_n_particle, doubleVector_t(m_n_dimensions)};
+
     // Dynamic Variables
+    doubleMatrix_t forces{m_n_particle, doubleVector_t(m_n_dimensions)};
+
+    // Energetic Variables
+
+
 
     // Trivial Default Constructor
 public:
     Simulation() = default;
 
 
+    // Constructor to input simulation parameters
+public:
+    Simulation(int num_iter, int num_simulations,
+               unsigned long n_particle, double temp,
+               double box, double epsilon,
+               double sigma, double dt) :
+            m_num_iter(num_iter), m_num_simulations(num_simulations),
+            m_temp(temp), m_box(box),
+            m_epsilon(epsilon), m_sigma(sigma),
+            m_dt(dt), m_t_total(dt * m_num_iter) {
+        // Nothing to do here
+    }
+
+
     // Print values to test
-    void toPrint( std::vector<std::vector<double> > &vector)
+private:
+    void toPrint(doubleMatrix_t &vector)
     {
         // Loop through all rows
-        for ( std::vector<std::vector<int>>::size_type i = 0; i < vector.size(); i++ )
+        for (intMatrix_t::size_type i = 0; i < vector.size(); i++)
         {
             // Loop through all columns (in each row)
-            for ( std::vector<int>::size_type j = 0; j < vector[i].size(); j++ )
+            for (intVector_t::size_type j = 0; j < vector[i].size(); j++)
             {
                 std::cout << vector[i][j] << "\t"; // Print the value at that location (i,j)
             }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+
 #include "Simulation_Class.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
     // Make class with uniform initialization
-    Simulation sim_test {};
+    Simulation sim_test_default{};
+
+    // Simulation variables
+    int num_iter = 2000;
+    int num_simulations = 1;
+    unsigned long n_particle = 50;
+    double temp = 1.0;
+    double box = 5.0;
+    double epsilon = 1.0;
+    double sigma = 1.0;
+    double dt = 1e-4;
+    // Initialize class
+    Simulation sim_test_simVarConst{num_iter, num_simulations,
+                                    n_particle, temp,
+                                    box, epsilon,
+                                    sigma, dt};
+
 
     return 0;
 }
