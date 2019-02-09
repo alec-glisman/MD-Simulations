dc8b967f5f08825e6a9821a759bee37ad32ae90e
Alec Glisman <alec.glisman@gmail.com>
2019-02-08 19:05:03 -0800

    Added -O3 compiler optimization flag to cmake file. Program now much even faster

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     // Simulation variables
-    long num_iter = 1500;
+    long num_iter = 15000;
     int num_simulations = 1;
-    unsigned long n_particle = 1000;
-    double temp = 1.0;
+    unsigned long n_particle = 600;
+    double temp = 5.0;
     double box = 15.0;
-    double epsilon = 3.0;  // Depth of the potential well
-    double sigma = 1.0;  // (Max) Length over which interactions can occur
-    double dt = 0.005;
+    double epsilon = 2.0;  // Depth of the potential well
+    double sigma = 2.0;  // (Max) Length over which interactions can occur
+    double dt = 0.001;
     std::string filename = "01";
     std::string foldername = "Simulation-Output";
     int n_dump = 10; // Dump XYZ frame every n iterations
